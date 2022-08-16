4f59564631a9dc66304dea9e7b0a2081488a094b
NAFIZ009 <jalalnafiz3@gmail.com>
2022-08-17 01:52:37 +0600

    sec

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 let logInfo=[
     {
-        email:"jalalnafiz3@gmail.com",
-        password:"jalalnafiz"
-    }
+        email:"tomarMail@gmail.com",
+        password:"duhkteDao"
+    },
+    {
+        email:"amrMail@gmail.com",
+        password:"duhkteDaoPart2"
+    },
+    {
+        email:"shobarMail@gmail.com",
+        password:"duhkteDaoPart3"
+    },
+    {
+        email:"awamiMail@gmail.com",
+        password:"duhkteDaoPart4"
+    },
+    {
+        email:"bnpMail@gmail.com",
+        password:"duhkteDaoPart5"
+    },
 ]
 document.getElementById("btn").addEventListener("click", function(e) {
     let password=document.getElementById("pass").value;
     let email=document.getElementById("mail").value;
-    if (password==logInfo[0].password && email==logInfo[0].email) {
-        window.location.replace("main/main.html");
-        document.getElementById("warningPass").classList.add("d-none");
-        document.getElementById("warningMail").classList.add("d-none");
-        document.getElementById("success").classList.remove("d-none");
-
-    }
-    if (password!=logInfo[0].password) {
-        document.getElementById("warningPass").classList.remove("d-none");
-    }
-    if (email!=logInfo[0].email) {
-        document.getElementById("warningMail").classList.remove("d-none");
-    }
+    logInfo.forEach(function(item) {
+        if (password==item.password && email==item.email) {
+            window.location.replace("main/main.html");
+            document.getElementById("warningPass").classList.add("d-none");
+            document.getElementById("warningMail").classList.add("d-none");
+            document.getElementById("success").classList.remove("d-none");
+    
+        }
+        if (password!=item.password) {
+            document.getElementById("warningPass").classList.remove("d-none");
+        }
+        if (email!=item.email) {
+            document.getElementById("warningMail").classList.remove("d-none");
+        }
+    });
 });
